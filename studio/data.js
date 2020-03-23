8906ac1382ddd9679456f5877de6477c3fe560fc
frransier <frransier@gmail.com>
2020-03-23 14:11:01 +0100

    new data

--- studio/data.js
+++ studio/data.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("covidsverige0322.json"));
+const data = JSON.parse(fs.readFileSync("covidsverige0323.json"));
 const coordinates = JSON.parse(fs.readFileSync("geoSverige.json"));
 const _ = require("lodash");
 
